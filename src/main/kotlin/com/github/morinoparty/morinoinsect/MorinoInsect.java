0cab728d0129b969a5853810045143641f5b997c
Unitarou <atelierkisame@gmail.com>
2021-02-26 11:38:30 +0900

    Initial commit

---
+++ src/main/kotlin/com/github/morinoparty/morinoinsect/MorinoInsect.java
@@ -0,0 +1,17 @@
+package com.github.morinoparty.morinoinsect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MorinoInsect extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
