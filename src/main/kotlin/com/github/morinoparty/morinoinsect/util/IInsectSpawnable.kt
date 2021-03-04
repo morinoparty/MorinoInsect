96ccac2b0806237df8b027ba463075f94a067b66
sota70 <sota52350@gmail.com>
2021-03-04 23:22:21 +0900

    :sparkles: Add sample.yml file

--- src/main/kotlin/com/github/morinoparty/morinoinsect/util/IInsectSpawnable.kt
+++ src/main/kotlin/com/github/morinoparty/morinoinsect/util/IInsectSpawnable.kt
@@ -8,5 +8,8 @@ interface IInsectSpawnable {
     var Id: Int
     var Tier: InsectTier
 
-    fun spawn(spawnLocation: Location)
+    fun spawn(spawnLocation: Location) {
+        // Todo: スポーンメソッドの処理を書く（今は適当なやつにしてる）
+        println(Name + " has just spawned to " + spawnLocation.x + ", " + spawnLocation.y + ", " + spawnLocation.z + " ID: " + Id + " Tier: " + Tier)
+    }
 }
