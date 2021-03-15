b41c31737611c1f2ee20d1bb27d2fa10adeb4fe2
sota70 <sota52350@gmail.com>
2021-03-15 20:24:51 +0900

    :shirt: Change class properties and spawn method

--- src/main/kotlin/com/github/morinoparty/morinoinsect/insectslist/InsectBase.kt
+++ src/main/kotlin/com/github/morinoparty/morinoinsect/insectslist/InsectBase.kt
@@ -1,6 +1,8 @@
 package com.github.morinoparty.morinoinsect.insectslist
 
 import com.github.morinoparty.morinoinsect.MorinoInsect
+import com.github.morinoparty.morinoinsect.condition.SpawnTypeCondition
+import com.github.morinoparty.morinoinsect.condition.SpawnableBlockCondition
 import com.github.morinoparty.morinoinsect.extentions.selectOneRandomly
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
@@ -29,27 +31,26 @@ interface InsectBase {
     /** 虫のサイズの最大値 */
     val lengthMax: Int
 
-    // ToDo: 虫のスポーンブロック指定はmore-fishのconditionとして扱う
     /** 虫がスポーンできるブロック */
-    val spawnableBlock: Material
+    val spawnableBlock: SpawnableBlockCondition
 
     /**
      * スポーンタイプ
      * どういうスポーンの仕方をするかを指定する
      */
-    val spawnType: SpawnType
+    val spawnType: SpawnTypeCondition
 
     /** 虫の見た目 */
     val icon: ItemStack
 
     /**
-     * 虫を指定したブロックの位置に指定したスポーンタイプでスポーンさせる
+     * 虫を指定したブロックの位置に透明の額縁に入れて指定したスポーンタイプでスポーンさせる
      *
      * @param[player] 対象のプレイヤー
      * @param[spawnBlock] スポーンブロック
      */
     fun spawn(player: Player, spawnBlock: Block) {
-        if (spawnType == SpawnType.ONGROUND) {
+        if (spawnType.checkCondition(SpawnType.ONGROUND)) {
             val itemFrame = player.world.spawn(spawnBlock.location, ItemFrame::class.java)
             // 額縁を透明にして壊れないようにする
             val invisible = NamespacedKey(MorinoInsect.getInstance(), "invisible")
@@ -59,12 +60,12 @@ interface InsectBase {
             itemFrame.persistentDataContainer.set(invulnerable, PersistentDataType.BYTE, 1)
             itemFrame.setFacingDirection(itemFrameDirection)
             itemFrame.setItem(icon)
-        } else if (spawnType == SpawnType.ONTREE) {
+        } else if (spawnType.checkCondition(SpawnType.ONTREE)) {
             val itemFrame = player.world.spawn(spawnBlock.location, ItemFrame::class.java)
             // 額縁を透明にして壊れないようにする
             val invisible = NamespacedKey(MorinoInsect.getInstance(), "invisible")
             val invulnerable = NamespacedKey(MorinoInsect.getInstance(), "invulnerable")
-            val itemFrameDirection = getAppropriateDirection(spawnBlock)
+            val itemFrameDirection = getUnburiedDirection(spawnBlock)
             itemFrame.persistentDataContainer.set(invisible, PersistentDataType.BYTE, 1)
             itemFrame.persistentDataContainer.set(invulnerable, PersistentDataType.BYTE, 1)
             if (itemFrameDirection == null) return
@@ -78,8 +79,8 @@ interface InsectBase {
      * @param[spawnBlock] 対象のスポーンブロック
      * @return スポーンブロックの他のブロックに埋もれてない面からランダムに選んだ１つの面
      */
-    private fun getAppropriateDirection(spawnBlock: Block): BlockFace {
-        val appropriateDirection: MutableList<BlockFace> = ArrayList()
+    private fun getUnburiedDirection(spawnBlock: Block): BlockFace {
+        val unburiedDirection: MutableList<BlockFace> = ArrayList()
         val northBlockNextTo = spawnBlock.getRelative(BlockFace.NORTH)
         val southBlockNextTo = spawnBlock.getRelative(BlockFace.SOUTH)
         val eastBlockNextTo = spawnBlock.getRelative(BlockFace.EAST)
@@ -89,11 +90,11 @@ interface InsectBase {
         val isEastBlockAir = eastBlockNextTo.type == Material.AIR
         val isWestBlockAir = westBlockNextTo.type == Material.AIR
         when {
-            isNorthBlockAir == true -> { appropriateDirection.add(BlockFace.NORTH) }
-            isSouthBlockAir == true -> { appropriateDirection.add(BlockFace.SOUTH) }
-            isEastBlockAir == true -> { appropriateDirection.add(BlockFace.EAST) }
-            isWestBlockAir == true -> { appropriateDirection.add(BlockFace.WEST) }
+            isNorthBlockAir == true -> { unburiedDirection.add(BlockFace.NORTH) }
+            isSouthBlockAir == true -> { unburiedDirection.add(BlockFace.SOUTH) }
+            isEastBlockAir == true -> { unburiedDirection.add(BlockFace.EAST) }
+            isWestBlockAir == true -> { unburiedDirection.add(BlockFace.WEST) }
         }
-        return appropriateDirection.selectOneRandomly()
+        return unburiedDirection.selectOneRandomly()
     }
 }
