ca1a7852bbe4e0fad34daee8d13423d1e42f4d94
sota70 <sota52350@gmail.com>
2021-03-29 20:06:24 +0900

    :sparkles: Add parameter named spawnType to pickRandomType method

--- src/main/kotlin/com/github/morinoparty/morinoinsect/catching/insect/InsectTypeTable.kt
+++ src/main/kotlin/com/github/morinoparty/morinoinsect/catching/insect/InsectTypeTable.kt
@@ -29,11 +29,12 @@ data class InsectTypeTable(
     fun pickRandomType(
         catcher: Player,
         block: Material,
+        spawnType: String
         // TODO: ディレクションチェックのために引数が増える予定
     ): InsectType? {
         val types = insectMap.values.filter {
             it.rarity == pickRandomRarity().first && it.conditions.generateConditionSet()
-                .all { condition -> condition.check(catcher, block) }
+                .all { condition -> condition.check(catcher, block, spawnType) }
         }
         if (types.isEmpty()) return null
         return types.random()
