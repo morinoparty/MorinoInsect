d0f2fb1f7b40a2063f295f033783f97505e5a0d2
sota70 <sota52350@gmail.com>
2021-03-29 20:22:56 +0900

    :shirt: Remove 6 files inside insectslist package

--- src/main/kotlin/com/github/morinoparty/morinoinsect/insectslist/InsectTier.kt
+++
@@ -1,6 +0,0 @@
-package com.github.morinoparty.morinoinsect.insectslist
-
-class InsectTier(tierName_: String) {
-
-    val tierName: String = tierName_.toUpperCase()
-}
--- src/main/kotlin/com/github/morinoparty/morinoinsect/insectslist/SpawnType.kt
+++
@@ -1,7 +0,0 @@
-package com.github.morinoparty.morinoinsect.insectslist
-
-/** 虫のスポーンタイプの種類を定義しているクラス */
-enum class SpawnType {
-    ONGROUND,
-    ONTREE
-}
