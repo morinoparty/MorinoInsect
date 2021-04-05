c6873ff125f652ce21a14b2f95b400889d4b40af
sota70 <sota52350@gmail.com>
2021-04-05 16:39:44 +0900

    :sparkles: Add player location completion

--- src/main/kotlin/com/github/morinoparty/morinoinsect/MorinoInsect.kt
+++ src/main/kotlin/com/github/morinoparty/morinoinsect/MorinoInsect.kt
@@ -8,9 +8,15 @@ import com.github.morinoparty.morinoinsect.catching.insect.InsectTypeTable
 import com.github.morinoparty.morinoinsect.command.MainCommand
 import com.github.morinoparty.morinoinsect.configuration.Config
 import com.github.morinoparty.morinoinsect.item.InsectItemStackConverter
+import org.apache.commons.lang.Validate
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.block.BlockFace
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
+import org.bukkit.util.StringUtil
+import java.util.ArrayList
 
 class MorinoInsect : KotlinPlugin() {
     init { Config.load(this) }
@@ -34,8 +40,62 @@ class MorinoInsect : KotlinPlugin() {
         completions.registerAsyncCompletion("blocks") {
             Material.values().filter { it.isSolid }.map { block -> block.toString().toLowerCase() }
         }
-        completions.registerAsyncCompletion("spawnType") {
+        completions.registerAsyncCompletion("spawnTypes") {
             BlockFace.values().map { it.toString().toLowerCase() }
+        }
+        completions.registerAsyncCompletion("playerLocX") { c ->
+            val sender: CommandSender = c.sender
+            Validate.notNull(sender, "Sender cannot be null")
+            val senderPlayer = if (sender is Player) sender as Player else null
+            val matchedPlayers = ArrayList<String>()
+            for (player in Bukkit.getOnlinePlayers()) {
+                val name = player.name
+                val playerLocX = player.location.x.toString()
+                if ((senderPlayer == null || senderPlayer.canSee(player)) && StringUtil.startsWithIgnoreCase(
+                        name,
+                        c.input
+                    )
+                ) {
+                    matchedPlayers.add(playerLocX)
+                }
+            }
+            matchedPlayers
+        }
+        completions.registerAsyncCompletion("playerLocY") { c ->
+            val sender: CommandSender = c.sender
+            Validate.notNull(sender, "Sender cannot be null")
+            val senderPlayer = if (sender is Player) sender as Player else null
+            val matchedPlayers = ArrayList<String>()
+            for (player in Bukkit.getOnlinePlayers()) {
+                val name = player.name
+                val playerLocY = player.location.y.toString()
+                if ((senderPlayer == null || senderPlayer.canSee(player)) && StringUtil.startsWithIgnoreCase(
+                        name,
+                        c.input
+                    )
+                ) {
+                    matchedPlayers.add(playerLocY)
+                }
+            }
+            matchedPlayers
+        }
+        completions.registerAsyncCompletion("playerLocZ") { c ->
+            val sender: CommandSender = c.sender
+            Validate.notNull(sender, "Sender cannot be null")
+            val senderPlayer = if (sender is Player) sender as Player else null
+            val matchedPlayers = ArrayList<String>()
+            for (player in Bukkit.getOnlinePlayers()) {
+                val name = player.name
+                val playerLocZ = player.location.z.toString()
+                if ((senderPlayer == null || senderPlayer.canSee(player)) && StringUtil.startsWithIgnoreCase(
+                        name,
+                        c.input
+                    )
+                ) {
+                    matchedPlayers.add(playerLocZ)
+                }
+            }
+            matchedPlayers
         }
     }
 
