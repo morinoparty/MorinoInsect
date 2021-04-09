05116bc3d6818b6838862d1cf0ec86d6aaedaa2c
sota70 <sota52350@gmail.com>
2021-04-09 09:54:57 +0900

    :sparkles: Almost complete spawn insects process

--- src/main/kotlin/com/github/morinoparty/morinoinsect/catching/SpawningInsectsListener.kt
+++ src/main/kotlin/com/github/morinoparty/morinoinsect/catching/SpawningInsectsListener.kt
@@ -4,18 +4,17 @@ import com.github.morinoparty.morinoinsect.MorinoInsect
 import com.github.morinoparty.morinoinsect.catching.area.AreaChecker
 import com.github.morinoparty.morinoinsect.catching.area.Vector
 import com.github.morinoparty.morinoinsect.catching.insect.Insect
+import com.github.morinoparty.morinoinsect.catching.insect.SpawnType
+import org.bukkit.Material
 import org.bukkit.block.Block
-import org.bukkit.block.BlockFace
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.ItemFrame
+import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
-import org.jetbrains.annotations.NotNull
-import org.jetbrains.annotations.Nullable
-import java.lang.IllegalStateException
 
 /** 虫のスポーン処理をするイベントクラス */
 class SpawningInsectsListener(
@@ -35,59 +34,56 @@ class SpawningInsectsListener(
             player.inventory.itemInMainHand == catchingNet
         if (!playerHasInsectCatchingNet) return
         val spawnBlock = detectBlocksAroundPlayer(player).random()
-        val randomSpawnType = BlockFace.values().random()
-        spawnInsect(player, spawnBlock, randomSpawnType)
+        val spawnType = SpawnType.values().random()
+        spawnInsect(player, spawnBlock, spawnType)
     }
 
     // 虫をランダムに選んでスポーンさせるメソッド
-    private fun spawnInsect(@NotNull catcher: Player, spawnBlock: Block, spawnType: BlockFace) {
+    private fun spawnInsect(catcher: Player, spawnBlock: Block, spawnType: SpawnType) {
         val insect: Insect = plugin.insectTypeTable.pickRandomType(
             catcher = catcher,
             block = spawnBlock,
             spawnType = spawnType
-        )?.generateInsect() ?: throw IllegalStateException("条件に当てはまる虫はありませんでした")
+        )?.generateInsect() ?: return
         val insectItem = plugin.converter.createItemStack(catcher, insect)
-        val insectItemFrame: ItemFrame =
-            (catcher.world.spawnEntity(spawnBlock.location, EntityType.ITEM_FRAME) as ItemFrame).also {
-                it.setItem(insectItem)
-                it.setFacingDirection(spawnType)
-                it.isVisible = true
-            }
+        val insectItemFrame: ItemFrame = (
+            catcher.world.spawnEntity(
+                spawnBlock.location.add(0.0, 1.0, 0.0),
+                EntityType.ITEM_FRAME
+            ) as ItemFrame
+            ).also {
+            it.setItem(insectItem)
+            it.setFacingDirection(spawnType.direction)
+        }
+
+        // 額縁にランダムに選んだ虫を付けるために一度ItemFrameにキャストする必要がある
+        // ToDo: insectItemFrameにキャストできないエラーを治す
+        (insectItemFrame as LivingEntity).also {
+            it.isInvisible = true
+            it.isInvulnerable = true
+        }
+
+        // デバッグ用
+        println("${insectItemFrame}がスポーンしました！")
     }
 
     // プレイヤーの周り（半径３ブロックの円）にあるブロックを検知して返すメソッド
-    @Nullable
-    private fun detectBlocksAroundPlayer(@NotNull player: Player): MutableList<Block> {
+    private fun detectBlocksAroundPlayer(player: Player): MutableSet<Block> {
+        val playerLocBlock = player.location.block
+        val detectArea = Vector(-3..3, -1..3, -3..3)
+        val detectedBlocksSet: MutableSet<Block> = mutableSetOf()
 
-        // プレイヤーの真下のブロックの位置を取得したいため、y座標を-1した
-        val playerLocation = player.location.subtract(0.0, 1.0, 0.0)
-        val leftDetectArea = Vector(3, 3, 3)
-        val rightDetectArea = Vector(-3, 3, -3)
-        val detectedBlocks: MutableList<Block> = mutableListOf()
-
-        // 全体の検出範囲の左側を検出して検知したブロックをListに入れる
-        for (x in 0..leftDetectArea.x step 1) {
-            for (z in 0..leftDetectArea.z step 1) {
-                for (y in 0..leftDetectArea.y step 1) {
-                    val areaChecker = AreaChecker(x, z)
-                    if (areaChecker.isSquareCorner()) continue
-                    val detectedLocation = playerLocation.add(x.toDouble(), y.toDouble(), z.toDouble())
-                    detectedBlocks.add(detectedLocation.block)
+        // detectAreaで指定した範囲のブロックを検出してdetectedBlocksSetに入れる
+        for (x in detectArea.x) {
+            for (y in detectArea.y) {
+                for (z in detectArea.z) {
+                    if (AreaChecker(x, z).isSquareCorner()) continue
+                    val detectedBlock = playerLocBlock.getRelative(x, y, z)
+                    if (detectedBlock.type == Material.AIR) continue
+                    detectedBlocksSet.add(detectedBlock)
                 }
             }
         }
-
-        // 全体の検出範囲の右側を検出して検知したブロックをListに入れる
-        for (x in 0..rightDetectArea.x step -1) {
-            for (z in 0..rightDetectArea.z step -1) {
-                for (y in 0..rightDetectArea.y step 1) {
-                    val areaChecker = AreaChecker(x, z)
-                    if (areaChecker.isSquareCorner()) continue
-                    val detectedLocation = playerLocation.add(x.toDouble(), y.toDouble(), z.toDouble())
-                    detectedBlocks.add(detectedLocation.block)
-                }
-            }
-        }
-        return detectedBlocks
+        return detectedBlocksSet
     }
 }
