d1ce4848c956fbde11933fa174d80db47513a1ad
sota70 <sota52350@gmail.com>
2021-04-09 09:52:40 +0900

    :shirt: Change debug code to release one

--- src/main/kotlin/com/github/morinoparty/morinoinsect/catching/insect/InsectCatchingNet.kt
+++ src/main/kotlin/com/github/morinoparty/morinoinsect/catching/insect/InsectCatchingNet.kt
@@ -4,7 +4,6 @@ import com.github.morinoparty.morinoinsect.configuration.toComponent
 import com.github.morinoparty.morinoinsect.item.edit
 import com.github.morinoparty.morinoinsect.util.NamespacedKeyUtils
 import kotlinx.serialization.Serializable
-import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 
@@ -18,8 +17,7 @@ data class InsectCatchingNet(
 ) {
     fun createInsectNet(): ItemStack {
         val material = NamespacedKeyUtils.material(id)
-        val itemStack = ItemStack(Material.CLOCK)
-        Material.matchMaterial("minecraft:clock")
+        val itemStack = ItemStack(material)
         itemStack.edit<ItemMeta> {
             displayName(name.toComponent())
             setCustomModelData(this@InsectCatchingNet.customModelData)
