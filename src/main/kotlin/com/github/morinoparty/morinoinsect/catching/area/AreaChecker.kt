0beadac2a93963f2ecf9a2f36ef6a85c38f5c257
sota70 <sota52350@gmail.com>
2021-04-09 10:34:42 +0900

    :shirt: Change class name AreaCoordinates to AreaLocation

--- src/main/kotlin/com/github/morinoparty/morinoinsect/catching/area/AreaChecker.kt
+++ src/main/kotlin/com/github/morinoparty/morinoinsect/catching/area/AreaChecker.kt
@@ -25,40 +25,40 @@ class AreaChecker(private val x: Int, private val y: Int) {
     // 座標がスポーン範囲の右上の角かどうか確かめるメソッド
     private fun isRightUpCorner(x: Int, y: Int): Boolean {
         val rightUpCorner = arrayOf(
-            AreaCoordinates(2, 3),
-            AreaCoordinates(3, 3),
-            AreaCoordinates(3, 2)
+            AreaLocation(2, 3),
+            AreaLocation(3, 3),
+            AreaLocation(3, 2)
         )
-        return AreaCoordinates(x, y) in rightUpCorner
+        return AreaLocation(x, y) in rightUpCorner
     }
 
     // 座標がスポーン範囲の右下の角かどうか確かめるメソッド
     private fun isRightDownCorner(x: Int, y: Int): Boolean {
         val rightDownCorner = arrayOf(
-            AreaCoordinates(2, -3),
-            AreaCoordinates(3, -2),
-            AreaCoordinates(3, -3)
+            AreaLocation(2, -3),
+            AreaLocation(3, -2),
+            AreaLocation(3, -3)
         )
-        return AreaCoordinates(x, y) in rightDownCorner
+        return AreaLocation(x, y) in rightDownCorner
     }
 
     // 座標がスポーン範囲の左上の角かどうか確かめるメソッド
     private fun isLeftUpCorner(x: Int, y: Int): Boolean {
         val leftUpCorner = arrayOf(
-            AreaCoordinates(-2, 3),
-            AreaCoordinates(-3, 2),
-            AreaCoordinates(-3, 3)
+            AreaLocation(-2, 3),
+            AreaLocation(-3, 2),
+            AreaLocation(-3, 3)
         )
-        return AreaCoordinates(x, y) in leftUpCorner
+        return AreaLocation(x, y) in leftUpCorner
     }
 
     // 座標がスポーン範囲の左下の角かどうか確かめるメソッド
     private fun isLeftDownCorner(x: Int, y: Int): Boolean {
         val leftDownCorner = arrayOf(
-            AreaCoordinates(-2, -3),
-            AreaCoordinates(-3, -2),
-            AreaCoordinates(-3, -3)
+            AreaLocation(-2, -3),
+            AreaLocation(-3, -2),
+            AreaLocation(-3, -3)
         )
-        return AreaCoordinates(x, y) in leftDownCorner
+        return AreaLocation(x, y) in leftDownCorner
     }
 }
