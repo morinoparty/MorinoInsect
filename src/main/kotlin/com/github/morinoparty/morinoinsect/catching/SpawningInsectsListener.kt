ecbd61751360fcfb3a6d49808a7da9539b5ca6fa
sota70 <sota52350@gmail.com>
2021-04-10 17:12:16 +0900

    :sparkles: Add spawn insect message and fix condition when players click insectcatchingnet

--- src/main/kotlin/com/github/morinoparty/morinoinsect/catching/SpawningInsectsListener.kt
+++ src/main/kotlin/com/github/morinoparty/morinoinsect/catching/SpawningInsectsListener.kt
@@ -11,6 +11,7 @@ import org.bukkit.entity.ItemFrame
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
 
@@ -30,7 +31,10 @@ class SpawningInsectsListener(
         val player = event.player
         val playerHasInsectCatchingNet =
             player.inventory.itemInMainHand == catchingNet
+        val playerRightClicked =
+            event.action == Action.RIGHT_CLICK_BLOCK || event.action == Action.RIGHT_CLICK_AIR
         if (!playerHasInsectCatchingNet) return
+        if (!playerRightClicked) return
         val spawnBlock = detectBlocksAroundPlayer(player).random()
         val spawnType = SpawnDirection.values().random()
         spawnInsect(player, spawnBlock, spawnType)
@@ -42,7 +46,13 @@ class SpawningInsectsListener(
             catcher,
             spawnBlock,
             spawnDirection
-        )?.generateInsect() ?: return
+        )?.let {
+            sendInsectsFoundMessage(catcher)
+            it.generateInsect()
+        } ?: run {
+            sendNoInsectsFoundMessage(catcher)
+            return
+        }
         val insectItem = plugin.converter.createItemStack(catcher, insect)
         val insectItemFrame = catcher.world.spawn(
             spawnBlock.location.add(0.0, 1.0, 0.0),
@@ -52,6 +62,7 @@ class SpawningInsectsListener(
             it.setFacingDirection(spawnDirection.direction)
             it.isVisible = false
         }
+
         // デバッグ用
         println("${insectItemFrame}がスポーンしました！")
     }
@@ -69,10 +80,39 @@ class SpawningInsectsListener(
                     if (AreaChecker(x, z).isSquareCorner()) continue
                     val detectedBlock = playerLocBlock.getRelative(x, y, z)
                     if (detectedBlock.type == Material.AIR) continue
+                    if (!canBlockSpawnInsects(detectedBlock)) continue
                     detectedBlocksSet.add(detectedBlock)
                 }
             }
         }
         return detectedBlocksSet
+    }
+
+    // ブロックが虫をスポーンできる面があるかどうか確認するメソッド
+    private fun canBlockSpawnInsects(spawnBlock: Block): Boolean {
+        val relativeBlocks: MutableList<Block> = mutableListOf(
+            spawnBlock.getRelative(SpawnDirection.DOWN.direction, 1),
+            spawnBlock.getRelative(SpawnDirection.UP.direction, 1),
+            spawnBlock.getRelative(SpawnDirection.NORTH.direction, 1),
+            spawnBlock.getRelative(SpawnDirection.SOUTH.direction, 1),
+            spawnBlock.getRelative(SpawnDirection.EAST.direction, 1),
+            spawnBlock.getRelative(SpawnDirection.WEST.direction, 1)
+        )
+        var spawnableFaceCount = 0
+        relativeBlocks.forEach { block ->
+            if (block.type == Material.AIR) return@forEach
+            spawnableFaceCount++
+        }
+        return spawnableFaceCount >= 1
+    }
+
+    // 条件に当てはまる虫がいなかった時にプレイヤーに送信するメッセージメソッド
+    private fun sendNoInsectsFoundMessage(catcher: Player) {
+        catcher.sendMessage("虫はいないようだ...")
+    }
+
+    // 条件に当てはまる虫がいた時にプレイヤーに送信するメッセージメソッド
+    private fun sendInsectsFoundMessage(catcher: Player) {
+        catcher.sendMessage("耳を済ませていたら、虫の声が聞こえてきた...")
     }
 }
