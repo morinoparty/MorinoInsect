101bf2a244d76ebbd67f37e17b2713c13c611b5a
sota70 <sota52350@gmail.com>
2021-04-23 09:56:03 +0900

    :sparkles: Change the way of detecting blocks

--- src/main/kotlin/com/github/morinoparty/morinoinsect/catching/RightClickNetListener.kt
+++ src/main/kotlin/com/github/morinoparty/morinoinsect/catching/RightClickNetListener.kt
@@ -1,12 +1,11 @@
 package com.github.morinoparty.morinoinsect.catching
 
 import com.github.morinoparty.morinoinsect.MorinoInsect
-import com.github.morinoparty.morinoinsect.catching.area.AreaChecker
-import com.github.morinoparty.morinoinsect.catching.area.AreaRange
+import com.github.morinoparty.morinoinsect.catching.area.BlockDetector
+import com.github.morinoparty.morinoinsect.catching.area.BlockFilter
 import com.github.morinoparty.morinoinsect.catching.area.SpawnLocationAdjuster
 import com.github.morinoparty.morinoinsect.catching.insect.Insect
 import com.github.morinoparty.morinoinsect.catching.insect.SpawnDirection
-import org.bukkit.Material
 import org.bukkit.block.Block
 import org.bukkit.entity.ItemFrame
 import org.bukkit.entity.Player
@@ -20,7 +19,7 @@ import org.bukkit.inventory.ItemStack
 class RightClickNetListener(
     private val plugin: MorinoInsect,
     private val catchingNet: ItemStack
-) : Listener, SpawnLocationAdjuster {
+) : Listener, SpawnLocationAdjuster, BlockFilter {
 
     /**
      * プレイヤーが虫網を右クリックするのを感知して虫をスポーンさせるメソッド
@@ -34,10 +33,13 @@ class RightClickNetListener(
             player.inventory.itemInMainHand == catchingNet
         val playerRightClicked =
             event.action == Action.RIGHT_CLICK_BLOCK || event.action == Action.RIGHT_CLICK_AIR
-        if (!playerHasInsectCatchingNet) return
-        if (!playerRightClicked) return
-        if (!player.isSneaking) return
-        val spawnBlock = detectBlocksAroundPlayer(player).random()
+        if (!playerHasInsectCatchingNet &&
+            !playerRightClicked &&
+            !player.isSneaking
+        ) return
+        val detectBlocksRadius = 8
+        val spawnBlock =
+            BlockDetector(player.location, detectBlocksRadius).detectBlocksOfSphere().takeSpawnable().random()
         val spawnType = SpawnDirection.values().random()
         spawnInsect(player, spawnBlock, spawnType)
     }
@@ -69,45 +71,6 @@ class RightClickNetListener(
 
         // デバッグ用
         println("${insectItemFrame}がスポーンしました！")
-    }
-
-    // プレイヤーの周り（半径３ブロックの円）にあるブロックを検知して返すメソッド
-    private fun detectBlocksAroundPlayer(player: Player): MutableSet<Block> {
-        val playerLocBlock = player.location.block
-        val detectArea = AreaRange(-8..8, -1..8, -8..8)
-        val detectedBlocksSet: MutableSet<Block> = mutableSetOf()
-
-        // detectAreaで指定した範囲のブロックを検出してdetectedBlocksSetに入れる
-        for (x in detectArea.x) {
-            for (y in detectArea.y) {
-                for (z in detectArea.z) {
-                    if (AreaChecker(x, z).isSquareCorner()) continue
-                    val detectedBlock = playerLocBlock.getRelative(x, y, z)
-                    if (detectedBlock.type == Material.AIR) continue
-                    if (!canBlockSpawnInsects(detectedBlock)) continue
-                    detectedBlocksSet.add(detectedBlock)
-                }
-            }
-        }
-        return detectedBlocksSet
-    }
-
-    // ブロックが虫をスポーンできる面が１つでもあるかどうか確認するメソッド
-    private fun canBlockSpawnInsects(spawnBlock: Block): Boolean {
-        var spawnableFaceCount = 0
-        val relativeBlocks: MutableList<Block> = mutableListOf(
-            spawnBlock.getRelative(SpawnDirection.DOWN.direction, 1),
-            spawnBlock.getRelative(SpawnDirection.UP.direction, 1),
-            spawnBlock.getRelative(SpawnDirection.NORTH.direction, 1),
-            spawnBlock.getRelative(SpawnDirection.SOUTH.direction, 1),
-            spawnBlock.getRelative(SpawnDirection.EAST.direction, 1),
-            spawnBlock.getRelative(SpawnDirection.WEST.direction, 1)
-        )
-        relativeBlocks.forEach { block ->
-            if (block.type != Material.AIR) return@forEach
-            spawnableFaceCount++
-        }
-        return spawnableFaceCount >= 1
     }
 
     // 条件に当てはまる虫がいなかった時にプレイヤーに送信するメッセージメソッド
